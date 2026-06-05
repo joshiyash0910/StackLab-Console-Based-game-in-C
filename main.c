3a55bc6e3541c648e77c2b0292f5d701900423b2
joshiyash0910 <joshiyash0910@gmail.com>
2026-06-05 09:46:15 +0530

    Added more to main and also created struct and set difficulty fuction

--- main.c
+++ main.c
@@ -2,6 +2,45 @@
 #include <conio.h> // taken for getch() function
 #include <stdlib.h> // taken for system("cls") function
 
+#define n 10
+
+struct LinkedStk{
+    char data[n];
+    int top;
+    struct LinkedStk* next;
+}; // a linked stack structure to be used in both games
+
+int SetDifficulty() // set difficulty level of the game (both)
+{
+    system("cls");
+    printf("Please select a difficulty level:\n");
+    printf("1. Easy\n");
+    printf("2. Medium\n");
+    printf("3. Hard\n");
+    while (1)
+    {
+        char c = getch();
+        switch(c)
+        {
+            case '1':
+                printf("You have selected Easy difficulty.\n");
+                getch();
+                return 1;
+            case '2':
+                printf("You have selected Medium difficulty.\n");
+                getch();
+                return 2;
+            case '3':
+                printf("You have selected Hard difficulty.\n");
+                getch();
+                return 3;
+            default:
+                printf("Invalid option. Please try again.\n");
+                getch();
+
+        }
+    }
+}
 
 int main()
 {
@@ -37,11 +76,25 @@ int main()
         printf("3. Options\n");
         printf("4. Exit\n");
         char c = getch();
+        switch(c) // placeholder
+        {
+            case '1':
+                printf("You have selected Sort the Beads\n");
+                break;
+            case '2':
+                printf("You have selected Tower of Hanoi\n");
+                break;
+            case '3':
+                printf("You have selected Options\n");
+                break;
+            case '4':
+                printf("Exiting the game...\n");
+                return 0;
+            default:
+                printf("Invalid option. Please try again.\n");
+               
+        }
+        getch();
     }
     
-
-
-
-
-    return 0;
 }
